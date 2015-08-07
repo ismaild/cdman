b1e68637b3e15b68ab6872dcc1d7ac0aa1b50909
Ismail Dhorat <ismail@codiez.co.za>
2015-08-07 11:26:57 +0200

    rspec config

---
+++ spec/spec_helper.rb
@@ -0,0 +1,26 @@
+require 'factory_girl_rails'
+
+RSpec.configure do |config|
+
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.include FactoryGirl::Syntax::Methods
+
+  config.order = 'random'
+  config.color = true
+  config.formatter = :documentation
+
+  config.before(:suite) do
+    DatabaseCleaner.orm = :mongoid
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:each) { DatabaseCleaner[:mongoid].start }
+  config.after(:each) { DatabaseCleaner[:mongoid].clean }
+end
